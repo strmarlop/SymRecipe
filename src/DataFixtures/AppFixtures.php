a09a47e6b83f16e3f3cd32d515b488a597457d92
Esther Martinez <estmartinezl@gmail.com>
2023-06-10 09:21:31 +0200

    :tada: Fixtures with Faker done

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,14 +5,28 @@ namespace App\DataFixtures;
 use App\Entity\Ingredient;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
 
 class AppFixtures extends Fixture
 {
+    
     public function load(ObjectManager $manager): void
     {
-        $ingredient = new Ingredient;
-        
+        $faker = Factory::create('fr_FR'); //que sean données en FR
 
+
+        for ($i=1; $i <=50; $i++) 
+        { 
+            $ingredient = new Ingredient;
+            $ingredient ->setName($faker->word());
+            $ingredient->setPrice(mt_rand(0,100));
+
+            $manager->persist($ingredient);        
+        }
+        
+     
+        
 
         $manager->flush();
     }
--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -29,6 +29,13 @@ class Ingredient
     #[Assert\NotNull]
     private ?\DateTimeImmutable $createdAt = null;
 
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+
     public function getId(): ?int
     {
         return $this->id;
